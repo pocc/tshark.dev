0660a45c007993ea4c3d2628ab96ea82c24bfc1d
Ross Jacobs <rj@swit.sh>
2019-07-22 21:54:35 -0700

    Finished making pdf script

--- pdf/makepdf.py
+++ pdf/makepdf.py
@@ -8,17 +8,18 @@ Make sure that you wrote all files in content/ as it's possible
 for something malicious to happen in parsing it.
 """
 import glob
+import json
 import operator
 import os
 import platform
 import re
 import shutil
 import subprocess as sp
-import sys
-import tempfile
+
 
 import toml
 import yaml
+
 
 def check_env():
     """Make sure we have the right files available."""
@@ -31,6 +32,7 @@ def check_env():
     if not os.path.exists("config.toml"):
         raise Exception("Hugo `config.toml` file not found in root")
 
+
 def get_value_by_key(key, filename, text):
     """Get value for given regex key in text. 
     Throws an error if there are no results."""
@@ -38,17 +40,21 @@ def get_value_by_key(key, filename, text):
     try:
         value = re.findall(key + regex, text)[0]
     except IndexError as e:
-        print("ERROR:", filename, "does not contain the " + key + " key in toml/yaml front matter."
+        print("ERROR:", filename, "does not contain the " + key +
+              " key in toml/yaml front matter." +
               "\n\tRegex used: `" + key + regex + "`")
         exit(1)
     return value
 
+
 def parse_document(filetext: str) -> (dict,):
     """Parse a document into a frontmatter dict and remaining text as str."""
-    matches = re.findall(r"^(?:---\n([\s\S]*?)\n---|\+\+\+\n([\s\S]*?)\n\+\+\+|({\n[\s\S]*\n})\n)([\s\S]*)", filetext)[0]
+    matches = re.findall(r"^(?:---\n([\s\S]*?)\n---"
+                         r"|\+\+\+\n([\s\S]*?)\n\+\+\+"
+                         r"|({\n[\s\S]*\n})\n)([\s\S]*)", filetext)[0]
     if len(matches) == 0:
-        raise Exception("File has frontmatter in invalid format." + \
-            "\nSee https://gohugo.io/content-management/front-matter/.")
+        raise Exception("File has frontmatter in invalid format."
+                        "\nSee https://gohugo.io/content-management/front-matter/.")
     text = matches[-1]
     frontmatter = list(filter(None, matches[:-1]))[0]
     if matches[0]: # YAML
@@ -59,7 +65,8 @@ def parse_document(filetext: str) -> (dict,):
         return json.loads(frontmatter), text
     else:
         raise Exception("Regex failed. Please create an issue!")
-    
+
+
 def get_proj_params() -> (str, str):
     """Get hugo project params from config.toml."""
     with open("config.toml") as f:
@@ -68,6 +75,7 @@ def get_proj_params() -> (str, str):
     author = get_value_by_key("author", "config.toml", config_text)
     
     return title, author
+
 
 def install_pandoc():
     os_name = platform.system()
@@ -86,8 +94,9 @@ def install_pandoc():
     else:
         exit(1)
 
+
 def get_text_from_folder(folder: str, index: int) -> str:
-    """ Algorithm is to create a dict of weights and text so that they can be arranged.
+    """ Algorithm is to create a dict of weights and text to be arranged.
     This function checks one folder at a time, recursively
     
     If 2 files at the same level have the same weight, order by title
@@ -99,12 +108,14 @@ def get_text_from_folder(folder: str, index: int) -> str:
             filetext = f.read()
             fm, text = parse_document(filetext)
             if "weight" not in fm: 
-                raise Exception("In file", filename, "weight parameter is not set in front matter.")
+                raise Exception("In file", filename,
+                                "weight parameter is not set in front matter.")
             if isinstance(fm["weight"], str):
                 if not fm["weight"].isdigit():
-                    raise Exception("In file", filename, "weight `", fm["weight"], "` is not a number.")
+                    raise Exception("In file", filename, "weight `",
+                                    fm["weight"], "` is not a number.")
                 fm["weight"] = int(fm["weight"]) 
-            # Get rid of notices. At some point convert these to a tex library like bclogo
+            # Get rid of notices. TODO convert these to tex library like bclogo
             text = re.sub(r"{{% notice[\s\S]*?/notice %}}", "", text)
             file_datum = {
                     "name": filename,
@@ -117,19 +128,20 @@ def get_text_from_folder(folder: str, index: int) -> str:
                 # to ensure that it gets sorted to be first
                 file_datum["weight"] = 0  
             else:
-                # Decrease heading # by 1 to allow for chapters if ## is being used
+                # Decrease heading # by 1 so chapters exist
                 text = re.sub(r"\n##", "\n###", text)
+                file_datum["text"] = text
             file_data.append(file_datum)
 
     ordered_files = sorted(file_data, key=operator.itemgetter("weight"))
-    for f_dict in ordered_files:
+    for fdict in ordered_files:
         # Chapter sections at level 0 should be top level headers
-        is_index = os.path.basename(f_dict["name"]) == "_index.md"
+        is_index = os.path.basename(fdict["name"]) == "_index.md"
         if index < 2 and is_index:
-            combined_text += "\n# " + f_dict["title"]
+            combined_text += "\n# " + fdict["title"]
         else:
-            combined_text += "\n## " + f_dict["title"]
-        combined_text += "\n\n_" + f_dict["desc"] + "_\n" + f_dict["text"] + "\n"
+            combined_text += "\n## " + fdict["title"]
+        combined_text += "\n\n_" + fdict["desc"] + "_\n" + fdict["text"] + "\n"
     
     folder_data = {}
     for folder in glob.glob(folder + "/*/"):
@@ -148,31 +160,41 @@ def get_text_from_folder(folder: str, index: int) -> str:
 
     return combined_text
 
-def convert_to_pdf(filetext: str):
+
+def convert_to_pdf(title: str, filetext: str):
+    """Convert to pdf using pandoc."""
     print("INFO: Converting to PDF")
-    this_dir = os.getcwd()
-    with tempfile.NamedTemporaryFile() as temp_md:
-        temp_md.write(bytes(filetext, "utf-8"))
-        temp_md.flush()
-        shutil.copy(temp_md.name, "all.md")
-        cmds = """pandoc -N --template=template.tex {} --pdf-engine=xelatex --toc
- --variable mainfont="Palatino"
- --variable sansfont="Arial"
- --variable monofont="Menlo"
- --variable fontsize=12pt
- --variable version=2.0
- -o tshark_dev.pdf""".format(this_dir)
-        sp.call(cmds.split(' '))
+    sanitized_title = ''.join(c for c in title if c.isalnum() or c in "-_. ")
+    output_name = sanitized_title.replace(" ", "-")
+    input_file = "pdf/" + output_name + ".md"
+    output_file = "pdf/" + output_name + ".tex"
+    this_dir = os.getcwd() + '/pdf/'
+    with open(input_file, "w") as f:
+        f.write(filetext)
+        f.flush()
+    cmds = """pandoc -N --template={}template.tex {} \
+--pdf-engine=xelatex --toc \
+--variable mainfont="Palatino" \
+--variable sansfont="Arial" \
+--variable monofont="Menlo" \
+--variable fontsize=12pt \
+--variable version=2.0 \
+-o {}""".format(this_dir, input_file, output_file)
+    print("INFO: Calling pandoc with:\n", cmds, sep="")
+    sp.call(cmds.split(' '))
+
 
 def makepdf():
+    """Generate a pdf from folders containing markdown files."""
     check_env()
     title, author = get_proj_params()
     pandoc_path = shutil.which("pandoc")
     if not pandoc_path:
         install_pandoc()
-    all_text = "# " + title + "\n\nBy " + author + get_text_from_folder("content", 0)
+    all_text = "# " + title + "\n\nBy " + author + \
+               get_text_from_folder("content", 0)
+    convert_to_pdf(title, all_text)
 
-    convert_to_pdf(all_text)
 
 if __name__ == '__main__':
     makepdf()
